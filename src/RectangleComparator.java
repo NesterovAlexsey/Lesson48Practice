55116880966bdc4e1348270e23e201f5fedfe283
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-26 08:47:26 +0200

    add guardian condition for Square<0 to class RectangleComparator

--- src/RectangleComparator.java
+++ src/RectangleComparator.java
@@ -6,6 +6,9 @@ public class RectangleComparator implements Comparator<Rectangle> {
   public int compare(Rectangle a, Rectangle b) {
     int s1 = a.getLength() * a.getWidth();
     int s2 = b.getLength() * b.getWidth();
+    if ((s1 < 0) || (s2 < 0)) {
+      throw new IllegalArgumentException("Incorrect square value");
+    }
     return s1 - s2;
   }
 }
