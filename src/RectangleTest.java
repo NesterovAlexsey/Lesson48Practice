621c42553ef3221af9aae7645064ea4973813560
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-26 08:30:11 +0200

    add test class RectangleTest, negativeWidthArgument()

---
+++ src/RectangleTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RectangleTest {
+
+  @Test
+  public void negativeWidthArgument(){
+    boolean flag = false;
+
+    try {
+      Rectangle first = new Rectangle(-1, 2);
+    } catch (IllegalArgumentException e) {
+      flag = true;
+    }
+    assertTrue(flag);
+  }
+
+  
+}
